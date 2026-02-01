d92821a4c48e6c86acaab275640e9163242efc83
elysekenny <elyse2.kenny@live.uwe.ac.uk>
2026-02-01 10:14:31 +0000

    i made the ui  a prefab
    i removed it from the scene in case there is conflicts?

--- GGJ2026Unity/Assets/ScriptableObjects/Controllers/AbilityController.cs
+++ GGJ2026Unity/Assets/ScriptableObjects/Controllers/AbilityController.cs
@@ -10,6 +10,7 @@ public class AbilityController : MonoBehaviour
 {
     public GameObject Self; 
     public GameObject Hitbox;
+    public Canvas UI;
 
     //references to all the masks in the game
     [Header("Masks")]
