9452b7012fb99232d592aca97ad78116e572f783
elysekenny <elyse2.kenny@live.uwe.ac.uk>
2026-02-03 19:14:28 +0000

    backend inventory
    seperated the player inventory into its own controller which can store multiple masks. going to begin the frontend next :)

--- GGJ2026Unity/Assets/ScriptableObjects/Controllers/AbilityController.cs
+++ GGJ2026Unity/Assets/ScriptableObjects/Controllers/AbilityController.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -17,102 +12,15 @@ public class AbilityController : MonoBehaviour
 {
     public GameObject Self;
     public GameObject Hitbox;
+    public InventoryController PlayerInventory;
     public Canvas UI;
  
     public List<GameObject> IntersectingColliders = new List<GameObject>();
- 
-    //references to all the masks in the game
-    [Header("Masks")]
-    public NewMask[] AllMasks;
- 
-    [Header("Weapons")]
-    public NewWeapon[] AllWeapons;
-    public NewWeapon PlayerWeaponSlot1; //overwrite this if the mask unlocks a weapon
  
     [Header("Boss Only")]
     public NewMask[] UsableMasks;
     public NewWeapon EnemyWeapon;
  
- 
-    // Private attributes
-    private NewMask PlayerMaskSlot1;
-    private NewMask PlayerMaskSlot2;
- 
-    public void Start()
-    {
-        if(PlayerWeaponSlot1)
-        {
-            GameObject WeaponSlot = UI.transform.Find("BottomPanel").gameObject.transform.Find("WeaponSlot").gameObject;
-            UnityEngine.UI.Image Border = WeaponSlot.transform.Find("SlotOuter").gameObject.GetComponent<UnityEngine.UI.Image>();
-            UnityEngine.UI.Image Icon = WeaponSlot.transform.Find("SlotIcon").gameObject.GetComponent<UnityEngine.UI.Image>();
-            TextMeshProUGUI Text = WeaponSlot.transform.Find("WeaponName").gameObject.GetComponent<TextMeshProUGUI>();
- 
-            Border.color = PlayerWeaponSlot1.UIColour;
-            Icon.sprite = PlayerWeaponSlot1.WeaponIcon;
-            Text.text = PlayerWeaponSlot1.WeaponName;
-        }
-    }
- 
-    // player only
-    public void CollectMask(string _maskName, int _slotNumber)
-    {
-        foreach(NewMask Mask in AllMasks)
-        {
-            if(Mask.MaskName == _maskName)
-            {
-                // assign to correct slot (presumable an input from a ui choice)
-                if(_slotNumber == 1)
-                {
-                    PlayerMaskSlot1 = Mask;
- 
-                    GameObject MaskSlot = UI.transform.Find("BottomPanel").gameObject.transform.Find("MaskSlot1").gameObject;
-                    UnityEngine.UI.Image Border = MaskSlot.transform.Find("SlotOuter").gameObject.GetComponent<UnityEngine.UI.Image>();
-                    UnityEngine.UI.Image Icon = MaskSlot.transform.Find("SlotIcon").gameObject.GetComponent<UnityEngine.UI.Image>();
-                    TextMeshProUGUI Text = MaskSlot.transform.Find("AbilityName").gameObject.GetComponent<TextMeshProUGUI>();
-                    UnityEngine.UI.Image ButtonPrompt =  MaskSlot.transform.Find("SlotButton").gameObject.GetComponent<UnityEngine.UI.Image>();
- 
-                    Border.color = Mask.UIColour;
-                    Icon.sprite = Mask.MaskIcon;
-                    Text.text = Mask.AbilityName;
-                    ButtonPrompt.color = Mask.UIColour;
-                }  
- 
-                else
-                {
-                    PlayerMaskSlot2 = Mask;
-                    GameObject MaskSlot = UI.transform.Find("BottomPanel").gameObject.transform.Find("MaskSlot2").gameObject;
-                    UnityEngine.UI.Image Border = MaskSlot.transform.Find("SlotOuter").gameObject.GetComponent<UnityEngine.UI.Image>();
-                    UnityEngine.UI.Image Icon = MaskSlot.transform.Find("SlotIcon").gameObject.GetComponent<UnityEngine.UI.Image>();
-                    TextMeshProUGUI Text = MaskSlot.transform.Find("AbilityName").gameObject.GetComponent<TextMeshProUGUI>();
-                    UnityEngine.UI.Image ButtonPrompt =  MaskSlot.transform.Find("SlotButton").gameObject.GetComponent<UnityEngine.UI.Image>();
- 
- 
-                    Border.color = Mask.UIColour;
-                    Icon.sprite = Mask.MaskIcon;
-                    Text.text = Mask.AbilityName;
-                    ButtonPrompt.color = Mask.UIColour;
- 
-                }
- 
-                // check if the mask unlocks a new weapon
-                if (Mask.WeaponUnlocked)
-                {
-                    PlayerWeaponSlot1 = Mask.WeaponUnlocked;
- 
-                    GameObject WeaponSlot = UI.transform.Find("BottomPanel").gameObject.transform.Find("WeaponSlot").gameObject;
-                    UnityEngine.UI.Image Border = WeaponSlot.transform.Find("SlotOuter").gameObject.GetComponent<UnityEngine.UI.Image>();
-                    UnityEngine.UI.Image Icon = WeaponSlot.transform.Find("SlotIcon").gameObject.GetComponent<UnityEngine.UI.Image>();
-                    TextMeshProUGUI Text = WeaponSlot.transform.Find("WeaponName").gameObject.GetComponent<TextMeshProUGUI>();
- 
-                    Border.color = PlayerWeaponSlot1.UIColour;
-                    Icon.sprite = PlayerWeaponSlot1.WeaponIcon;
-                    Text.text = PlayerWeaponSlot1.WeaponName;
-                }
- 
-                return;
-            }
-        }
-    }
  
     private void RunMaskAbility(NewMask Mask)
     {
@@ -142,7 +50,7 @@ public class AbilityController : MonoBehaviour
  
     public void PlayerAttack()
     {
-       BasicAttack(PlayerWeaponSlot1);
+       BasicAttack(PlayerInventory.PlayerWeaponSlot1);
     }
  
     public void EnemyAttack()
@@ -158,19 +66,18 @@ public class AbilityController : MonoBehaviour
  
     public NewMask.AnimationState TriggerAbility1()
     {
-        if(!PlayerMaskSlot1){return NewMask.AnimationState.None;} // some kind of feedback that there is no ability?
-        RunMaskAbility(PlayerMaskSlot1);
+        if(!PlayerInventory.PlayerMaskSlot1){return NewMask.AnimationState.None;} // some kind of feedback that there is no ability?
+        RunMaskAbility(PlayerInventory.PlayerMaskSlot1);
  
-        return PlayerMaskSlot1.PlayAnimation;
+        return PlayerInventory.PlayerMaskSlot1.PlayAnimation;
     }
  
     public NewMask.AnimationState TriggerAbility2()
     {
-        if(!PlayerMaskSlot2){return NewMask.AnimationState.None;} // some kind of feedback that there is no ability?
-        RunMaskAbility(PlayerMaskSlot2);
+        if(!PlayerInventory.PlayerMaskSlot2){return NewMask.AnimationState.None;} // some kind of feedback that there is no ability?
+        RunMaskAbility(PlayerInventory.PlayerMaskSlot2);
  
-        return PlayerMaskSlot2.PlayAnimation;
- 
+        return PlayerInventory.PlayerMaskSlot2.PlayAnimation;
     }
  
     IEnumerator Delay(float Lifetime)
@@ -288,7 +195,7 @@ public class AbilityController : MonoBehaviour
         if(WeaponToUse.WeaponType == NewWeapon.Weapon.Melee)
         {
             // MELEE ATTACK
-            HandleWeaponDamage(PlayerWeaponSlot1);
+            HandleWeaponDamage(PlayerInventory.PlayerWeaponSlot1);
         }
         else if(WeaponToUse.WeaponType == NewWeapon.Weapon.Ranged)
         {
---
+++ GGJ2026Unity/Assets/ScriptableObjects/Controllers/InventoryController.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+
+public class InventoryController : MonoBehaviour
+{
+    [Header("Information")]
+    public GameObject Self;
+    public Canvas UI;
+
+    [Header("Masks")]
+    public NewMask[] AllMasks;
+    public NewMask PlayerMaskSlot1;
+    public NewMask PlayerMaskSlot2;
+ 
+    [Header("Weapons")]
+    public NewWeapon[] AllWeapons;
+    public NewWeapon PlayerWeaponSlot1; //overwrite this if the mask unlocks a weapon
+
+
+    private List<NewMask> AllPlayerOwnedMasks = new List<NewMask>();
+    private List<NewWeapon> AllPlayerOwnedWeapons = new List<NewWeapon>();
+
+    public void Start()
+    {
+        if(PlayerWeaponSlot1)
+        {
+            GameObject WeaponSlot        = UI.transform.Find("BottomPanel").gameObject.transform.Find("WeaponSlot").gameObject;
+            UnityEngine.UI.Image Border  = WeaponSlot.transform.Find("SlotOuter").gameObject.GetComponent<UnityEngine.UI.Image>();
+            UnityEngine.UI.Image Icon    = WeaponSlot.transform.Find("SlotIcon").gameObject.GetComponent<UnityEngine.UI.Image>();
+            TextMeshProUGUI Text         = WeaponSlot.transform.Find("WeaponName").gameObject.GetComponent<TextMeshProUGUI>();
+
+            Border.color     = PlayerWeaponSlot1.UIColour;
+            Icon.sprite      = PlayerWeaponSlot1.WeaponIcon;
+            Text.text        = PlayerWeaponSlot1.WeaponName;
+        }
+    }
+
+    private void SetUISlotMask(GameObject MaskSlot, NewMask MaskToEquip)
+    {
+        UnityEngine.UI.Image Border          = MaskSlot.transform.Find("SlotOuter").gameObject.GetComponent<UnityEngine.UI.Image>();
+        UnityEngine.UI.Image Icon            = MaskSlot.transform.Find("SlotIcon").gameObject.GetComponent<UnityEngine.UI.Image>();
+        TextMeshProUGUI Text                 = MaskSlot.transform.Find("AbilityName").gameObject.GetComponent<TextMeshProUGUI>();
+        UnityEngine.UI.Image ButtonPrompt    =  MaskSlot.transform.Find("SlotButton").gameObject.GetComponent<UnityEngine.UI.Image>();
+
+        Border.color         = MaskToEquip.UIColour;
+        Icon.sprite          = MaskToEquip.MaskIcon;
+        Text.text            = MaskToEquip.AbilityName;
+        ButtonPrompt.color   = MaskToEquip.UIColour;
+    }
+
+    private void SetUISlotWeapon(NewWeapon WeaponToEquip)
+    {
+        PlayerWeaponSlot1            = WeaponToEquip;   
+        GameObject WeaponSlot        = UI.transform.Find("BottomPanel").gameObject.transform.Find("WeaponSlot").gameObject;
+        UnityEngine.UI.Image Border  = WeaponSlot.transform.Find("SlotOuter").gameObject.GetComponent<UnityEngine.UI.Image>();
+        UnityEngine.UI.Image Icon    = WeaponSlot.transform.Find("SlotIcon").gameObject.GetComponent<UnityEngine.UI.Image>();
+        TextMeshProUGUI Text         = WeaponSlot.transform.Find("WeaponName").gameObject.GetComponent<TextMeshProUGUI>();
+
+        Border.color     = PlayerWeaponSlot1.UIColour;
+        Icon.sprite      = PlayerWeaponSlot1.WeaponIcon;
+        Text.text        = PlayerWeaponSlot1.WeaponName;   
+    }
+
+    //PUBLIC METHOD WHEN THE PLAYER CLICKS THE EQUIP BUTTON
+    public void EquipMask(NewMask MaskToEquip, int SlotNum)
+    {
+        if(SlotNum == 1){PlayerMaskSlot1 = MaskToEquip;}
+        else if(SlotNum == 2){PlayerMaskSlot2 = MaskToEquip;}
+
+        GameObject MaskSlot = UI.transform.Find("BottomPanel").gameObject.transform.Find("MaskSlot" + SlotNum).gameObject;
+        SetUISlotMask(MaskSlot, MaskToEquip);
+    }
+
+    //PUBLIC METHOD WHEN THE PLAYER CLICKS THE EQUIP BUTTON
+    public void EquipWeapon(NewWeapon WeaponToEquip)
+    {
+        PlayerWeaponSlot1 = WeaponToEquip;
+        SetUISlotWeapon(WeaponToEquip);
+    }
+
+    // PUBLIC METHOD WHEN THE PLAYER OBTAINS A MASK THROUGH GAMEPLAY
+    public void AddMaskToInventory(string _maskName)
+    {
+        foreach(NewMask Mask in AllMasks)
+        {
+            if(Mask.MaskName == _maskName)
+            { 
+                AllPlayerOwnedMasks.Add(Mask);
+                // check if the mask unlocks a new weapon
+                if (Mask.WeaponUnlocked) {AllPlayerOwnedWeapons.Add(Mask.WeaponUnlocked);}
+                return;
+            }
+        }
+    }
+}
